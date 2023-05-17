5c4e74f677c82547fac39b4ca2dc490f927a6ea0
skpar16 <skpar16@naver.com>
2023-05-17 11:02:02 +0900

    디바이스체크 주석처리

--- src/main/webapp/js/m/device_check.js
+++ src/main/webapp/js/m/device_check.js
@@ -1,16 +1,16 @@
 
-function deviceCheck(activeProfilesCode) {
-    if(activeProfilesCode == "LIVE") {
-        var currentUrl = window.location.href;
+// function deviceCheck(activeProfilesCode) {
+//     if(activeProfilesCode == "LIVE") {
+//         var currentUrl = window.location.href;
         
-          var pattern = /m\/main\.do/;
+//           var pattern = /m\/main\.do/;
         
-        if (pattern.test(currentUrl)) {
-            if (window.navigator.userAgent.indexOf("app_") == -1) {
-                alert("앱으로 접근해 주세요");
-                window.location.href = "https://play.google.com/store/apps/details?id=kr.co.mygb&hl=ko";
-            }
-        }
-    }
-}
+//         if (pattern.test(currentUrl)) {
+//             if (window.navigator.userAgent.indexOf("app_") == -1) {
+//                 alert("앱으로 접근해 주세요");
+//                 window.location.href = "https://play.google.com/store/apps/details?id=kr.co.mygb&hl=ko";
+//             }
+//         }
+//     }
+// }
 
